0daee40eefd224e43a5128ee9478683214acdddf
Brandon Sturgeon <brandon@brandonsturgeon.com>
2023-07-05 16:26:03 -0700

    fix

--- interactions/ext/persistence/__init__.py
+++ interactions/ext/persistence/__init__.py
@@ -1,4 +1,4 @@
-from .base import base, version, setup
+from interactions.ext.persistence.base import base, version, setup
 from .cipher import Cipher
 from .extension import (
     PersistenceExtension,
